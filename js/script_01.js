a0776e6f6861dabec6e5be628ff1d340b111ac39
elgato <amzfba.1bestbuy@gmail.com>
2022-09-14 16:34:13 +0200

    update

---
+++ js/script_01.js
@@ -0,0 +1,14 @@
+//parent manipulation css
+//hover w Javascript
+//activationn
+new hoverEffect({
+  parent: document.querySelector(".distortion"),
+  intensity: 0.2,
+  image1: "./images/project01/01.png",
+  image2: "./images/project01/02.png",
+  displacementImage: "./images/project01/diss.png",
+  imagesRatio: 280 / 300
+});
+console.log("j usqu'ici tout vas bien");
+
+console.log(hoverEffect);
